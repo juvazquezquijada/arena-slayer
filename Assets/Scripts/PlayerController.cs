389c838e8e2333114172d02ea5c08a8a40914f39
juvazquezquijada <121971788+juvazquezquijada@users.noreply.github.com>
2023-04-11 21:39:53 -0700

    made a seperate player script for shooting

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,26 +10,13 @@ public class PlayerController : MonoBehaviour
     private CharacterController controller;
     private Vector3 moveDirection = Vector3.zero;
 
-    //Properties of the Pump Shotgun
-    public Transform gunBarrel;
-    public GameObject bulletPrefab;
-    public float bulletSpeed = 50f;
-    public float fireRate = 0.1f;
-    public float bulletLifetime = 2f;
-    private float fireTimer = 0f;
-    public Animator shotgunAnimator;
-    private bool shotFired = false;
     //Audio clips the player makes
-    public AudioClip shootSound;
     public AudioClip deathSound;
     public AudioClip healthPickup;
-    public AudioClip ammoPickup;
     private AudioSource audioSource;
     public AudioClip hurtSound;
     //Status of the player
-    public int health = 100;
-    public int maxAmmo = 25;
-    private int currentAmmo = 0;
+    public int health = 100;   
     public int score = 1;
     public int points = 0;
     public bool isDead = false;
@@ -41,13 +28,11 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         //Get AudioSource component
-        audioSource = GetComponent<AudioSource>();
+        
         //Get CharacterController component
         controller = GetComponent<CharacterController>();
         rb = GetComponent<Rigidbody>();
-        currentAmmo = maxAmmo;
         canvasManager = CanvasManager.Instance;
-        CanvasManager.Instance.UpdateAmmo(currentAmmo);
         CanvasManager.Instance.UpdateHealth(health);
         CanvasManager.Instance.UpdateScore(score);
     }
@@ -91,55 +76,10 @@ public class PlayerController : MonoBehaviour
         //Move the character controller
         controller.Move(moveDirection * Time.deltaTime);
         }
-        //Shoot a bullet when the shoot button is pressed and ammo is more than 0 and the shooting cooldown is up 
-        if (Input.GetButtonDown("Fire1") && fireTimer <= 0f && currentAmmo > 0 && isDead == false)
-        {
-            PlayShootAnimations();
-
-            //Play shooting sound
-            audioSource.PlayOneShot(shootSound);
-
-            //Spawn bullet at gun barrel
-            GameObject bullet = Instantiate(bulletPrefab, gunBarrel.position, gunBarrel.rotation);
-
-            //Add velocity to the bullet
-            Rigidbody rb = bullet.GetComponent<Rigidbody>();
-            rb.velocity = transform.forward * bulletSpeed;
-
-            //Destroy bullet after 2 seconds
-            Destroy(bullet, bulletLifetime);
-
-            //Decrease the ammo stat
-            currentAmmo--;
-
-            //Update the ammo stat
-            CanvasManager.Instance.UpdateAmmo(currentAmmo);
-            
-            //Reset the fire cooldown
-            fireTimer = fireRate;
-        }
-
-        // Decrease the fire timer if the player can't shoot yet
-        if (fireTimer > 0f)
-        {
-            fireTimer -= Time.deltaTime;
-        }
+        
     }
 
-    public void SetShotFired(bool value)
-    {
-        shotFired = value;
-    }
-
-    public bool GetShotFired()
-    {
-        return shotFired;
-    }
-
-    public void ResetShotFired()
-    {
-        shotFired = false;
-    }
+    
     
     public void TakeDamage(int damage)
     {
@@ -150,22 +90,12 @@ public class PlayerController : MonoBehaviour
         }
     } 
 
-    private void PlayShootAnimations()
-    {
-        Animator anim = GetComponent<Animator>();
-        anim.SetTrigger("Shoot");
-        shotgunAnimator.SetTrigger("Shoot");
-    }
+    
     public void UpdateHealth (int health)
     {
         CanvasManager.Instance.UpdateHealth(health);
     }
     
-    void UpdateAmmo()
-    {
-    CanvasManager.Instance.UpdateAmmo(currentAmmo);
-    }
-
     void Die()
     {
             isDead = true;
@@ -180,14 +110,8 @@ public class PlayerController : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
 {
-   
-    if (other.gameObject.CompareTag("Bullet"))
-    {
-        currentAmmo = maxAmmo;
-        UpdateAmmo();
-        Destroy(other.gameObject);
-    }
-    else if (other.gameObject.CompareTag("Health"))
+
+    if (other.gameObject.CompareTag("Health"))
     {
         health += 25;
         if (health > 100) health = 100;
@@ -195,14 +119,7 @@ public class PlayerController : MonoBehaviour
         Destroy(other.gameObject);
         audioSource.PlayOneShot(healthPickup);
     }
-    else if (other.gameObject.CompareTag("Ammo"))
-    {
-        currentAmmo += 7;
-        if (currentAmmo > 25) currentAmmo = 25;
-        CanvasManager.Instance.UpdateAmmo(currentAmmo);
-        Destroy(other.gameObject);
-        audioSource.PlayOneShot(ammoPickup);
-    }
+    
     else if (other.gameObject.CompareTag("Fireball"))
     {
         health -= 5;
