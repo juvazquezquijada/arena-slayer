e0cb8bd4fc087d1a540a6e7493721b01439e81e3
dazheng6 <121971803+dazheng6@users.noreply.github.com>
2023-04-18 18:00:21 -0700

    Fix the cameras jitteriness when moving the mouse

--- Assets/Scripts/gameplay/Camera.cs
+++ Assets/Scripts/gameplay/Camera.cs
@@ -6,6 +6,7 @@ public class Camera : MonoBehaviour
 {
     public float sensitivity = 100.0f;
     public float clampAngle = 80.0f;
+    public float smoothness = 0.1f;
 
     public Transform firstPersonTransform;
     public Transform thirdPersonTransform;
@@ -14,56 +15,63 @@ public class Camera : MonoBehaviour
     private float rotY = 0.0f;
     private float rotX = 0.0f;
 
+    private Quaternion desiredRotation;
+
     void Start()
     {
         Application.targetFrameRate = 60;
         Vector3 rot = transform.localRotation.eulerAngles;
         rotY = rot.y;
         rotX = rot.x;
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
-
         currentTransform = firstPersonTransform;
+        desiredRotation = transform.rotation;
+    }
 
-        if (CanvasManager.Instance.gameActive) 
+    void Update()
+    {
+        if (CanvasManager.Instance.gameActive)
         {
-            Cursor.visible = true;
-        } 
-        else 
-        {
+            Cursor.lockState = CursorLockMode.Locked;
             Cursor.visible = false;
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
         }
     }
 
     void FixedUpdate()
-{
-    float mouseX = Input.GetAxis("Mouse X");
-    float mouseY = -Input.GetAxis("Mouse Y");
-
-    if (Input.GetKeyDown(KeyCode.V))
     {
-        if (currentTransform == firstPersonTransform)
+        float mouseX = Input.GetAxisRaw("Mouse X");
+        float mouseY = -Input.GetAxisRaw("Mouse Y");
+
+        if (Input.GetKeyDown(KeyCode.V))
         {
-            currentTransform = thirdPersonTransform;
+            if (currentTransform == firstPersonTransform)
+            {
+                currentTransform = thirdPersonTransform;
+            }
+            else
+            {
+                currentTransform = firstPersonTransform;
+            }
         }
-        else
-        {
-            currentTransform = firstPersonTransform;
-        }
+
+        rotY += mouseX * sensitivity * Time.deltaTime;
+        rotX += mouseY * sensitivity * Time.deltaTime;
+
+        rotX = Mathf.Clamp(rotX, -clampAngle, clampAngle);
+
+        Quaternion localRotation = Quaternion.Euler(rotX, rotY, 0.0f);
+        desiredRotation = localRotation;
+
+        transform.position = currentTransform.position;
+
+        // Set the player's rotation based on the camera's rotation
+        transform.parent.rotation = Quaternion.Euler(0.0f, rotY, 0.0f);
+
+        // Smoothly interpolate between the current rotation and the desired rotation
+        transform.rotation = Quaternion.Slerp(transform.rotation, desiredRotation, smoothness);
     }
-
-    rotY += mouseX * sensitivity * Time.fixedDeltaTime;
-    rotX += mouseY * sensitivity * Time.fixedDeltaTime;
-
-    rotX = Mathf.Clamp(rotX, -clampAngle, clampAngle);
-
-    Quaternion localRotation = Quaternion.Euler(rotX, rotY, 0.0f);
-    transform.rotation = localRotation;
-
-    transform.position = currentTransform.position;
-
-    // Set the player's rotation based on the camera's rotation
-    transform.parent.rotation = Quaternion.Euler(0.0f, rotY, 0.0f);
-}
-
 }
