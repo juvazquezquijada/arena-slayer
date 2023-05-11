c353b55af03d543c909d7f0eb38b70a0a481532a
juvazquezquijada <121971788+juvazquezquijada@users.noreply.github.com>
2023-05-10 21:08:21 -0700

    updated tutorial text

--- Assets/Scripts/gameplay/PlayerController.cs
+++ Assets/Scripts/gameplay/PlayerController.cs
@@ -146,7 +146,7 @@ public class PlayerController : MonoBehaviour
     
     else if (other.gameObject.CompareTag("Fireball"))
     {
-        health -= 5;
+        health -= 6;
         if (health < 0) health = 0;
         CanvasManager.Instance.UpdateHealth(health);
         Destroy(other.gameObject);
@@ -175,7 +175,7 @@ public class PlayerController : MonoBehaviour
     }
     else if (other.gameObject.CompareTag("EnemyProjectile"))
     {
-        health -= 3;
+        health -= 7;
         if (health < 0) health = 0;
         CanvasManager.Instance.UpdateHealth(health);
         audioSource.PlayOneShot(hurtSound);
